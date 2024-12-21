ed406d774df0d54bdf7b7b3f9bc5a18f5ef9f633
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-12-21 12:33:32 -0500

    add the game logic

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
-
+import json
 import asyncio
 
 from websockets.asyncio.server import serve
 from websockets.exceptions import ConnectionClosedOK
+from connect4 import PLAYER1, PLAYER2, Connect4
 
 async def handler(websocket):
-    async for message in websocket:
-        try:
-            print(message)
-        except ConnectionClosedOK:
-            print("Connection closed.")
-            break
+    for player, column, row in [
+        (PLAYER1, 3, 0),
+        (PLAYER2, 3, 1),
+        (PLAYER1, 4, 0),
+        (PLAYER2, 4, 1),
+        (PLAYER1, 2, 0),
+        (PLAYER2, 1, 0),
+        (PLAYER1, 5, 0),
+    ]:
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        await websocket.send(json.dumps(event))
+        # 7 moves appear at .5 second intervals.
+        await asyncio.sleep(.5)
+    # async for message in websocket:
+    #     try:
+    #         print(message)
+    #     except ConnectionClosedOK:
+    #         print("Connection closed.")
+    #         break
 
 
 
--- main.js
+++ main.js
@@ -16,11 +16,43 @@ function sendMoves(board, websocket) {
   });
 }
 
+function showMessage(message) {
+  // this is used because window.alert is synchronous, would screw up the game
+  window.setTimeout(() => window.alert(message), 50);
+}
+
+function receiveMoves(board, websocket) {
+  websocket.addEventListener("message", ({ data }) => {
+    const event = JSON.parse(data);
+    switch (event.type) {
+      case "play":
+        // update UI with move
+        playMove(board, event.player, event.column, event.row);
+        break;
+      case "win":
+        showMessage(`Player ${event.player} won!`);
+        websocket.close(1000);
+        break;
+      case "error":
+        showMessage(event.message);
+        break;
+      default:
+        throw new Error(`Unknown event type: ${event.type}`);
+    }
+  });
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   // Initialize the UI.
   const board = document.querySelector(".board");
   createBoard(board);
   // open websocket connection and register event handlers
   const websocket = new WebSocket("ws://localhost:8001/");
+  receiveMoves(board, websocket);
   sendMoves(board, websocket);
+});
+
+websocket.addEventListener("message", ({ data }) => {
+  const event = JSON.parse(data);
+  // do something with event
 });
